61e9da57d2f0d7cdde5457933c8e156bf5accced
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-01-29 23:26:17 +0900

    まだ動く状態ではないがとりあえずmain.jsの設定を先にしておく

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+//import VueHead from 'vue-head'; header要素
 import App from './App.vue'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import router from './router';
 
 Vue.config.productionTip = false
 
+firebaseConfig = {
+  apiKey: "***********",
+  authDomain: "***********:",
+  databaseURL: "***********",
+  projectId: "**************",
+  storageBucket: "**********",
+  messagingSenderId: "***********:"
+};
+
+firebase.initializeApp(firebaseConfig);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
