b274be21a566bb59dc9283f07b1fd9d9d53c4604
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-01-30 22:01:01 +0900

    import firebase/analytics

--- src/main.js
+++ src/main.js
@@ -4,18 +4,11 @@ import App from './App.vue'
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import router from './router';
+import 'firebase/analytics';
 
 Vue.config.productionTip = false
 
 const firebaseConfig = {
-  apiKey: "***",
-  authDomain: "***",
-  databaseURL: "***",
-  projectId: "***",
-  storageBucket: "***",
-  messagingSenderId: "***",
-  appId: "***",
-  measurementId: "***"
 };
 
 firebase.initializeApp(firebaseConfig);
