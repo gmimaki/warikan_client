cae0a56db88c2b8fb84d928f57b08defd1844e2f
岳人三牧 <gmimakiwithkamejiroandnana@gmail.com>
2020-11-23 21:29:02 +0900

    inviter vuex-persistedstate

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import createPersistedState from 'vuex-persistedstate';
 
 Vue.use(Vuex);
 
@@ -16,5 +17,9 @@ export default new Vuex.Store({
         userId(state) {
             return state.userId;
         }
-    }
-})
+    },
+    plugins: [createPersistedState({
+        key: "userId",
+        storage: window.localStorage,
+    })]
+});
